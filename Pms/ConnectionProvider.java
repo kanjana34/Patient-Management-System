16c517076aedde01167770195dd67133c2ff85f1
kanjana34 <anjana12garg@gmail.com>
2021-11-26 15:59:45 +0530

    Added files for first time

---
+++ Pms/ConnectionProvider.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pms;
+import java.sql.*;
+
+/**
+ *
+ * @author kanuPriya
+ */
+public class ConnectionProvider {
+     
+ 
+    public static Connection getCon()
+    {
+        try
+        {
+         Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/pms","root","admin");
+            return con;
+            
+        }
+        catch(Exception e)
+        {
+          return null; 
+        }
+    }
+    
+}
